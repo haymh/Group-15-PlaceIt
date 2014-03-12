8770db0c0b2c24b5cf6f66c9844ff5125016f204
haymh <hengmeng2009@gmail.com>
2014-03-11 21:08:50 -0700

    change create method in createCategory page

--- src/com/fifteen/placeit/CreateCategoryPIActivity.java
+++ src/com/fifteen/placeit/CreateCategoryPIActivity.java
@@ -1,17 +1,15 @@
 package com.fifteen.placeit;
 
-import java.util.Collections;
 import java.util.Date;
-import java.util.Vector;
 
 import com.fifteen.placeit.WeeklySchedule.NumOfWeekRepeat;
 import com.google.android.gms.maps.model.LatLng;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.SparseBooleanArray;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -23,11 +21,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class CreateCategoryPIActivity extends Activity{
+public class CreateCategoryPIActivity extends Activity implements AdapterView.OnItemClickListener {
 	
 	private Button cancel = null;
-	private Button create = null;
-	private Vector<String> vector=new Vector<String>();
 	private static String cat[] = {"accounting", "airport", "amusement_park","aquarium","art_gallery","atm","bakery",
 									"bank","bar","beauty_salon","bicycle_store","book_store","bowling_alley","bus_station",
 									"cafe","campground","car_dealer","car_rental","car_repair","car_wash","casino","cemetery",
@@ -43,13 +39,14 @@ public class CreateCategoryPIActivity extends Activity{
 		                            "shoe_store","shopping_mall","spa","stadium","storage","store","subway_station","synagogue",
 		                            "taxi_stand","train_station","travel_agency","university","veterinary_care","zoo"};
 	private ListView listview = null;
+	private CheckBox checkbox = null;
 	static int index = 0;
 	
 	private Date createDate = new Date();
 	private Date postDate = new Date();
 	private NumOfWeekRepeat numOfWeekRepea;
 	// temp
-	static private String[] catArray = new String[3];
+	static private String[] tempArray = new String[1];
 	
 	private ServiceManager serviceManager;
 	private MyService service;
@@ -58,25 +55,26 @@ public class CreateCategoryPIActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_category_pi);
 	    
-		vector.setSize(3);
-		vector.clear();
-		// listview adapter 		
+		// listview adapter 
+		
 		listview = (ListView)findViewById(R.id.cat_listview);
 		//listview = getListView();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, cat);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.cat_single_row, R.id.catView, cat);
 		listview.setAdapter(adapter);
-		listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+		
+		//listview.setItemsCanFocus(false);
+		//this.getListView().setSelector(R.drawable.selector);
+		
+		listview.setOnItemClickListener(this);
 		
 		
 		
-		
-		// create button
-		create = (Button)findViewById(R.id.buttonCreate);
-		create.setOnClickListener(new createListener());
 		// cancel button
 		cancel = (Button)findViewById(R.id.buttonCancel);
 		cancel.setOnClickListener(new cancelListener());
 		
+		// check box
+		//checkbox = (CheckBox)findViewById(R.id.checkbox);
 		
 		// Instantiate service manager
 		serviceManager = new ServiceManager(this);
@@ -110,61 +108,94 @@ public class CreateCategoryPIActivity extends Activity{
 			finish();
 		}		
 	}
-	class createListener implements OnClickListener{
-
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			SparseBooleanArray sba = listview.getCheckedItemPositions();
-			int counter = 0;
-			for(int i=0; i<sba.size();i++)
-			{
-				if(sba.valueAt(i))
-					counter++;
-			}
-			if(counter>3 || counter==0)
-			{
-				if(counter>3){
-					Toast.makeText(getApplicationContext(), "You've selected more than 3 categories.",
-							Toast.LENGTH_SHORT).show();
-				}
-				else
-				{
-					Toast.makeText(getApplicationContext(), "You didn't select any category.",
-							Toast.LENGTH_SHORT).show();
-				}
-			}
-			else
-			{
-				for(int i=0; i<sba.size();i++)
-				{
-					if(sba.valueAt(i) && vector.size()<3)
-					{
-						//Toast.makeText(getApplicationContext(), cat[sba.keyAt(i)],Toast.LENGTH_SHORT).show();
-						vector.add(cat[sba.keyAt(i)]);
-					}
-				}
-				Collections.sort(vector);
-				vector.copyInto(catArray);			
-
-				create(v);
-			}
-
-		}	
-		
-	}
 
 	// FIXME Working on this
 	public void create(View view){
+		// only for testing
+		tempArray[0] = cat[index];
+		/*
+		boolean s = service.createPlaceIt(cat[index], null, 0, 0, null, createDate,
+				postDate, 0, 0, AbstractPlaceIt.Status.ON_MAP, tempArray);
+		*/
 
-		boolean s = service.createPlaceIt(cat[index], "Testing Categories", 0, 0, 
-				WeeklySchedule.NumOfWeekRepeat.ZERO, createDate, postDate, new LatLng(0,0), AbstractPlaceIt.Status.ON_MAP, catArray );
+		
+		//		null, createDate, postDate, new LatLng(0, 0), AbstractPlaceIt.Status.ON_MAP, tempArray );
+		// TODO REPLACE THIS TOP
+		/* 
+		public boolean createPlaceIt(String title, String description, int repeatedDayInWeek, int repeatedMinute, 
+				WeeklySchedule.NumOfWeekRepeat numOfWeekRepeat, Date createDate, Date postDate, LatLng coordinate,
+				AbstractPlaceIt.Status status, String[] categories){
+		*/
+		final ProgressDialog dialog = ProgressDialog.show(this,
+				"Posting Data...", "Please wait...", false);
+		
+		new AsyncTask<Void, Void, Boolean>(){
 
-		if(s){
-			Toast.makeText(this, "New Place-it Created", Toast.LENGTH_SHORT).show();
-			this.finish();
-		}else
-			Toast.makeText(this, "New Place-it was not Created", Toast.LENGTH_SHORT).show();
-		}
+			@Override
+			protected Boolean doInBackground(Void... params) {
+				// TODO Auto-generated method stub
+				return service.createPlaceIt(cat[index], "Testing Categories", 0, 0, 
+						WeeklySchedule.NumOfWeekRepeat.ZERO, createDate, postDate, new LatLng(0,0), AbstractPlaceIt.Status.ON_MAP, tempArray );
+			}
+			
+			@Override
+			protected void onPostExecute(Boolean params) {
+				dialog.dismiss();
+				if(params){
+					Toast.makeText(CreateCategoryPIActivity.this, "New Category Place-it Created", Toast.LENGTH_SHORT).show();
+					CreateCategoryPIActivity.this.finish();
+				}else
+					Toast.makeText(CreateCategoryPIActivity.this, "New Category Place-it was not Created", Toast.LENGTH_SHORT).show();
+			}
+			
+		}.execute();
+		dialog.show();
+	}
+		
+	/*
+	public void onclick(View view) {
+		
+		Log.wtf("Something", "It works");
+		//CheckBox c = (CheckBox) findViewById(R.id.cat_listview);
+		myFancyMethod(view);
+		
+	}
+	
+	private void myFancyMethod(View view) {
+		ListView temp = (ListView) findViewById(R.id.cat_listview);
+		Toast.makeText(this, temp.getCount(), Toast.LENGTH_SHORT).show();
+	}*/
+
+	/*
+	public boolean validate()
+	{
+		int count=0;
+		
+	}
+
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int i,
+			long l) {
+		Log.wtf("Something", "It works");
+		//Checkable child = (Checkable) parent.getChildAt(i);
+		//child.toggle();
+		CheckBox temp = (CheckBox) view;
+		Toast.makeText(this, temp.getText()+" is selected "+ i, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		
+	}*/
+
+	@Override
+	public void onItemClick(AdapterView<?> l, View v, int i, long arg3) {
+
+		//TextView temp = (TextView) v;
+		Toast.makeText(this, "number "+ i+" is selected which is "+cat[i], Toast.LENGTH_SHORT).show();
+		index = i;
+		this.create(v);
+		finish();
+	}
 }
-
